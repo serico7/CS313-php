4099c06cfd302ff4ac4fac75da6f78a1f7a05d14
serico7 <srconley@pacbell.net>
2019-09-27 22:01:22 -0700

    made the heading link to pages and stand out

--- web/prove_02/header.php
+++ web/prove_02/header.php
@@ -2,8 +2,8 @@
 echo '
 <div class="row title"><div class="col">Sean Conley\'s Site</div></div>
 <div class="row">
-  <div class="col">Main Page</div>
-  <div class="col">Assignments</div>
+  <div class="col sub"><a href="mainpage.php">Main Page</a></div>
+  <div class="col sub"><a href="assignments.php">Assignments</a></div>
 </div>
 
 '?>
