1476846e4090fb00811c05138b3ada1bebc0edad
serico7 <srconley@pacbell.net>
2019-09-27 21:27:01 -0700

    added some color and library condensing

--- web/prove_02/header.php
+++ web/prove_02/header.php
@@ -1,8 +1,9 @@
 <?php
 echo '
+<div class="row title"><h2>Sean Conley\'s Site</h2></div>
 <div class="row">
-  <div class="col">.col</div>
-  <div class="col">.col</div>
+  <div class="col">Main Page</div>
+  <div class="col">Assignments</div>
 </div>
 
 '?>
