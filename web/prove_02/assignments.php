cdb7daa0b33d5ede76020676d909c28477b0cba4
serico7 <srconley@pacbell.net>
2019-09-27 20:30:21 -0700

    remaking assignment files as php files

--- web/prove_02/assignments.php
+++ web/prove_02/assignments.php
@@ -10,10 +10,14 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<script language="php">include 'header.php';</script>
+	<?php include 'header.php';?>
+	<div>
 	<a href="../hello.html">Prove 01 - Hello World</a>
+	</div><div>
 	<a href="../teach_02/firstteach.html">Teach 02 - Clientside Review</a>
+	</div><div>
 	<a href="mainpage.html">Prove 02 - Main Page</a>
+	</div>
 	<p> More coming soon...</p>
 </body>
 </html>
