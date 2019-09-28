47a0909f97cdf6a42dd4009cc9faa41d4ecaf4fa
serico7 <srconley@pacbell.net>
2019-09-28 13:49:24 -0700

    added content to the main page, reduced overhead using remote hosting for media

--- web/prove_02/mainpage.php
+++ web/prove_02/mainpage.php
@@ -8,5 +8,11 @@
 </head>
 <body>
 	<?php include 'header.php';?>
+	<img class="center" src="https://cdn.discordapp.com/attachments/620930841828261898/627597642708287493/prof_pic.PNG" alt="My Picture"><br/>
+	<p> My name is Sean Conley. I am from San Francisco, California. I am a student at Brigham Young University - Idaho working on a Computer Science degree. I am a huge nerd about pretty much every art medium. </p><br/>
+	<p>Enjoy a good song. <a href="https://boris.bandcamp.com/album/pink-deluxe-edition">Support the artist here.</a> Better yet, see them live if you ever have the opportunity, they were phenomenal.</p>
+	<audio controls class="center">
+    	<source src="https://t4.bcbits.com/stream/a05fc87ae83370ab8be104870ff09820/mp3-128/2428376225?p=0&ts=1569789692&t=cabcc0fd82070fdc03b8f547826425d0fe149dbd&token=1569789692_bb8e8294f975c335d50f8d83aa524ada7488ea3e" type="audio/mpeg">
+	</audio>
 </body>
 </html>
