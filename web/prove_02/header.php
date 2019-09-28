4a9fe83647cb0f508f33c705551b4c4750210096
serico7 <srconley@pacbell.net>
2019-09-27 21:53:00 -0700

    fixed typos, reorganized heading

--- web/prove_02/header.php
+++ web/prove_02/header.php
@@ -1,6 +1,6 @@
 <?php
 echo '
-<div class="row title">Sean Conley\'s Site</div>
+<div class="row title"><div class="col">Sean Conley\'s Site</div></div>
 <div class="row">
   <div class="col">Main Page</div>
   <div class="col">Assignments</div>
