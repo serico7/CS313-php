08315ebe5075ae9d13424db5fe5d444e829eda55
serico7 <srconley@pacbell.net>
2019-10-11 20:37:32 -0700

    put up the database code

---
+++ db/myDb.sql
@@ -0,0 +1,33 @@
+CREATE table works(
+	workid serial NOT NULL PRIMARY KEY,
+	name varchar(63) NOT NULL
+);
+
+CREATE table characters(
+	charid serial NOT NULL PRIMARY KEY,
+	name varchar(31) NOT NULL,
+	art varchar(255) NOT NULL
+);
+
+CREATE table worktocharacter(
+	workid integer NOT NULL references works(workid),
+	charid integer NOT NULL references characters(charid)
+);
+
+CREATE table persons(
+	userid serial NOT NULL PRIMARY KEY,
+	username varchar(31) NOT NULL UNIQUE,
+	password varchar(31) NOT NULL
+);
+
+CREATE table selectedworks(
+	userid integer NOT NULL references persons(userid),
+	workid integer NOT NULL references works(workid),
+	isIncluded boolean NOT NULL
+);
+
+CREATE table rankedchars(
+	userid integer NOT NULL references persons(userid),
+	charid integer NOT NULL references characters(charid),
+	userRank integer
+);
