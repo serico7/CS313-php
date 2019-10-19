947a69c4708071bebfb208c198c23f68fe19dc4b
serico7 <srconley@pacbell.net>
2019-10-19 02:31:21 -0700

    test

--- web/project_01/sort.php
+++ web/project_01/sort.php
@@ -8,11 +8,19 @@ $db = get_db();
 <head>
 	<title>Sort</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<script>
+		$( function() {
+			$( "#sortable" ).sortable();
+			$( "#sortable" ).disableSelection();
+		} );
+</script>
 </head>
 
 <body>
 
-<table>
+<ul id="sortable">
 <?php
 $id=1;
 $statement = $db->prepare("SELECT why.charid, exs.name, exs.art FROM rankedchars AS why JOIN characters AS exs ON why.charid = exs.charid WHERE userid=:id AND isIncluded ORDER BY userRank, why.charid");
@@ -23,10 +31,10 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 	$name = $row['name'];
 	$art = $row['exs.art'];
 	$charid = $row['why.charid'];
-	echo "<tr><td> $name </td></tr>";
+	echo "<li> $name </li>";
 }
 ?>
-</table>
+</ul>
 
 </body>
 </html>
