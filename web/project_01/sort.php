c2f9fde0d3080ec127dc59bfd520ba30c9c1ab15
serico7 <srconley@pacbell.net>
2019-10-19 19:47:36 -0700

    test

--- web/project_01/sort.php
+++ web/project_01/sort.php
@@ -17,15 +17,11 @@ $db = get_db();
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-	<script>
-		$( function() {
-			$( "#sortable" ).sortable();
-			$( "#sortable" ).disableSelection();
-		} );
-</script>
 <script>
 $(document).ready(function(){
-  $('ul').sortable({
+	$( "#sortable" ).sortable();
+	$( "#sortable" ).disableSelection();
+	$('ul').sortable({
         axis: 'y',
         stop: function (event, ui) {
 	        var data = $(this).sortable('toArray');
@@ -36,7 +32,7 @@ $(document).ready(function(){
 
 <body>
 <form method="post" action="generate.php">
-<ul id="sortable">
+<ul id="sortable" >
 <?php
 $id=1;
 $statement = $db->prepare("SELECT why.charid, exs.name, exs.art FROM rankedchars AS why JOIN characters AS exs ON why.charid = exs.charid WHERE userid=:id AND isIncluded ORDER BY userRank, why.charid");
