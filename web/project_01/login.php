cd19fbcae40ee5cfd82f74d8455a54b3e01f80f9
serico7 <srconley@pacbell.net>
2019-10-19 20:28:23 -0700

    test

--- web/project_01/login.php
+++ web/project_01/login.php
@@ -60,7 +60,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: welcome.php");
+                            header("location: sort.php");
                         } else{
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";
