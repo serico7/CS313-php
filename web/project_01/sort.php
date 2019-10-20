b009696674b40be9e4a1a53a84ab19fdb5ee2ff6
serico7 <srconley@pacbell.net>
2019-10-19 19:51:21 -0700

    test

--- web/project_01/sort.php
+++ web/project_01/sort.php
@@ -26,7 +26,7 @@ $(document).ready(function(){
         stop: function (event, ui) {
 	        var data = $(this).sortable('toArray');
             $('order').value(data);
-});});
+}});});
 </script>
 </head>
 
