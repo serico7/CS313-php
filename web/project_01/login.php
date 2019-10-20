53216e891ddfdd8353a572509bb7bd722438d58e
serico7 <srconley@pacbell.net>
2019-10-19 20:27:08 -0700

    test

--- web/project_01/login.php
+++ web/project_01/login.php
@@ -50,7 +50,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                         $id = $row["id"];
                         $username = $row["username"];
                         $hashed_password = $row["password"];
-                        if(password_verify($password, $hashed_password)){
+                        if($password == $hashed_password){
                             // Password is correct, so start a new session
                             session_start();
                             
