319fa0924cd5431f2751b072451db68de475c981
serico7 <srconley@pacbell.net>
2019-10-19 19:38:13 -0700

    test

--- web/project_01/sort.php
+++ web/project_01/sort.php
@@ -23,25 +23,44 @@ $db = get_db();
 			$( "#sortable" ).disableSelection();
 		} );
 </script>
+<script>
+$(document).ready(function(){
+  $('ul').sortable({
+        axis: 'y',
+        stop: function (event, ui) {
+	        var data = $(this).sortable('toArray');
+            $('order').value(data);
+});
+</script>
 </head>
 
 <body>
-
+<form method="post" action="generate.php">
 <ul id="sortable">
 <?php
 $id=1;
 $statement = $db->prepare("SELECT why.charid, exs.name, exs.art FROM rankedchars AS why JOIN characters AS exs ON why.charid = exs.charid WHERE userid=:id AND isIncluded ORDER BY userRank, why.charid");
 $statement->bindValue(':id', $id, PDO::PARAM_INT);
 $statement->execute();
+$count = 0;
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
 	$name = $row['name'];
 	$art = $row['art'];
 	$charid = $row['charid'];
 	echo "<li id='$charid'> $name (<a href='$art'>X</a>)</li>";
+	$count = $count++;
 }
+    echo "</ul><input id='sortsize' type='hidden' value='$count' name='sortsize'></input>"
 ?>
-</ul>
-
+<input id='order' type='hidden' value='' name='order'></input>
+Number of images: <select class="selector" name="Imagenum">
+    <option value="1">Five</option>
+    <option value="2">Ten</option>
+    <option value="3">Fifteen</option>
+    <option value="4">Twenty</option>
+</select>
+<button type="submit"> Generate </button>
+</form>
 </body>
 </html>
