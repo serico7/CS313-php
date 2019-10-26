af0a66cae1847aef382972cb0ee27b22928e2a2d
serico7 <srconley@pacbell.net>
2019-10-25 18:32:41 -0700

    test

--- web/project_01/sort.php
+++ web/project_01/sort.php
@@ -119,8 +119,8 @@ Number of images: <select class="selector" name="Imagenum">
     	<option value="4">Twenty</option>
 	</select>
 	<button id="generate" class="update" type="submit"> Generate </button>
-</form id="works" action="sort.php">
-
+</form>
+<form id="works" action="sort.php">
 <?php
 $statement = $db->prepare("SELECT why.workid, why.isincluded, exs.name FROM selectedworks AS why JOIN works AS exs ON why.workid = exs.workid WHERE userid=:id ORDER BY why.workid");
 $statement->bindValue(':id', $id, PDO::PARAM_INT);
